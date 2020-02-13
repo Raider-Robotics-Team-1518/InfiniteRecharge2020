c35d7bebb9d574f65497485d832d5073e5bfe92f
Team1518 <skypantherstudios+frcstudent1@gmail.com>
2020-02-12 19:21:50 -0500

    add color wheel target color detection

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,12 +13,17 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.I2C;
 import frc.robot.subsystems.ColorWheel;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Turret;
 import frc.robot.commands.AimTurret;
 import frc.robot.OI;
 import frc.robot.commands.AutonomousTest;
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorMatch;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -32,17 +37,26 @@ public class Robot extends TimedRobot {
   private static final String kCustomAuto = "My Auto";
   // private String m_autoSelected;
   // private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  /* 
+  private final I2C.Port i2cPort = I2C.Port.kOnboard;
+  private final ColorSensorV3 colorSensor = new ColorSensorV3(i2cPort);
+  private final ColorMatch m_colorMatcher = new ColorMatch();
+  private final Color kBlueTarget = ColorMatch.makeColor(0.129, .428, .443);
+  private final Color kGreenTarget = ColorMatch.makeColor(.172, .577, .251);
+  private final Color kRedTarget = ColorMatch.makeColor(.519, .347, .133);
+  private final Color kYellowTarget = ColorMatch.makeColor(.319, .558, .124);  
+  */
   private static String gameData = "";
   public static DriveTrain m_driveTrain = new DriveTrain();
   private OI m_oi;
   // private AimTurret m_aTurret = new AimTurret();
   public static Turret m_turret = new Turret();
-  private ColorWheel m_colorWheel = new ColorWheel();
+  private ColorWheel m_colorWheel;
   CommandBase at;
 
   /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
    */
   @Override
   public void robotInit() {
@@ -52,15 +66,17 @@ public class Robot extends TimedRobot {
     // SmartDashboard.putData("Auto choices", m_chooser);
     m_oi = new OI();
     m_turret.init();
+    m_colorWheel = new ColorWheel();
   }
 
   /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like diagnostics that you want ran during disabled, autonomous,
+   * teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
@@ -69,14 +85,15 @@ public class Robot extends TimedRobot {
 
   /**
    * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
+   * between different autonomous modes using the dashboard. The sendable chooser
+   * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+   * remove all of the chooser code and uncomment the getString line to get the
+   * auto name from the text box below the Gyro
    *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
+   * <p>
+   * You can add additional auto modes by adding additional comparisons to the
+   * switch structure below with additional strings. If using the SendableChooser
+   * make sure to add them to the chooser code above as well.
    */
   @Override
   public void autonomousInit() {
@@ -88,7 +105,7 @@ public class Robot extends TimedRobot {
     at.initialize();
     at.execute();
     // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    
+
   }
 
   /**
@@ -96,20 +113,23 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousPeriodic() {
-  //   switch (m_autoSelected) {
-  //     case kCustomAuto:
-  //       // Put custom auto code here
-  //       break;
-  //     case kDefaultAuto:
-  //     default:
-  //       // Put default auto code here
-  //       break;
-  //   }
-    //SmartDashboard.putNumber("mFrontAvg", at.autosystem.a_drive.getFrontAverage());
-    //SmartDashboard.putNumber("mLeftAvg", at.autosystem.a_drive.getLeftStrafeAverage());
-    //SmartDashboard.putNumber("mRightAvg", at.autosystem.a_drive.getRightStrafeAverage());
-    //SmartDashboard.putNumber("mRearAvg", at.autosystem.a_drive.getRearAverage());
-  
+    // switch (m_autoSelected) {
+    // case kCustomAuto:
+    // // Put custom auto code here
+    // break;
+    // case kDefaultAuto:
+    // default:
+    // // Put default auto code here
+    // break;
+    // }
+    // SmartDashboard.putNumber("mFrontAvg",
+    // at.autosystem.a_drive.getFrontAverage());
+    // SmartDashboard.putNumber("mLeftAvg",
+    // at.autosystem.a_drive.getLeftStrafeAverage());
+    // SmartDashboard.putNumber("mRightAvg",
+    // at.autosystem.a_drive.getRightStrafeAverage());
+    // SmartDashboard.putNumber("mRearAvg", at.autosystem.a_drive.getRearAverage());
+
   }
 
   /**
@@ -118,50 +138,18 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
 
-    m_driveTrain.drive(Math.pow(m_oi.m_stick.getX(), 3), Math.pow(-m_oi.m_stick.getY(), 3), m_oi.m_stick.getZ()*.75);
+    m_driveTrain.drive(Math.pow(m_oi.m_stick.getX(), 3), Math.pow(-m_oi.m_stick.getY(), 3),
+        Math.pow(m_oi.m_stick.getZ(), 3));
 
     // For testing the thrower
-    //thrower.set(m_oi.m_stick.getThrottle());
+    // thrower.set(m_oi.m_stick.getThrottle());
 
     // For testing the turret ring (lazy susan)
     // m_turret.turretPivot.set(m_oi.m_stick.getThrottle());
 
-    
 
-    // Send color data to dashboard
-    SmartDashboard.putNumber("xRed", m_colorWheel.detectedColor.red);
-    SmartDashboard.putNumber("xGreen", m_colorWheel.detectedColor.green);
-    SmartDashboard.putNumber("xBlue", m_colorWheel.detectedColor.blue);
-    // m_aTurret.execute();  we found a loophole to check if this was working
+    // m_aTurret.execute(); we found a loophole to check if this was working
 
-    gameData = DriverStation.getInstance().getGameSpecificMessage();
-    if(gameData.length() > 0)
-    {
-      switch (gameData.charAt(0))
-      {
-        case 'B' :
-          //Blue case 
-          SmartDashboard.putString("FMS Color", "BLUE");
-          break;
-        case 'G' :
-          //Green case code
-          SmartDashboard.putString("FMS Color", "GREEN");
-          break;
-        case 'R' :
-          //Red case code
-          SmartDashboard.putString("FMS Color", "RED");
-          break;
-        case 'Y' :
-          //Yellow case code
-          SmartDashboard.putString("FMS Color", "YELLOW");
-          break;
-        default :
-          //This is corrupt data
-          break;
-      }
-    } else {
-      //Code for no data received yet
-    }
   }
 
   @Override
--- src/main/java/frc/robot/Subsystems/ColorWheel.java
+++ src/main/java/frc/robot/Subsystems/ColorWheel.java
@@ -10,17 +10,25 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorSensorV3;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 
 public class ColorWheel extends SubsystemBase {
   
   // Color Sensor
   private final I2C.Port i2cPort = I2C.Port.kOnboard;
-  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
-  public Color detectedColor = m_colorSensor.getColor();
+  private final ColorSensorV3 colorSensor = new ColorSensorV3(i2cPort);
+  private final ColorMatch m_colorMatcher = new ColorMatch();
+  private final Color kBlueTarget = ColorMatch.makeColor(0.129, .428, .443);
+  private final Color kGreenTarget = ColorMatch.makeColor(.172, .577, .251);
+  private final Color kRedTarget = ColorMatch.makeColor(.519, .347, .133);
+  private final Color kYellowTarget = ColorMatch.makeColor(.319, .558, .124);
 
   // Motor to rotate
   public static WPI_TalonSRX colorWheelMotor = new WPI_TalonSRX(5);
@@ -35,13 +43,70 @@ public class ColorWheel extends SubsystemBase {
   // Pneumatics to lift/lower
   
   public ColorWheel() {
+    m_colorMatcher.addColorMatch(kBlueTarget);
+    m_colorMatcher.addColorMatch(kGreenTarget);
+    m_colorMatcher.addColorMatch(kRedTarget);
+    m_colorMatcher.addColorMatch(kYellowTarget);
 
   }
 
   @Override
   public void periodic() {
     // Update color value from sensor to dashboard
-    detectedColor = m_colorSensor.getColor();
-
+        // Send color data to dashboard
+        Color detectedColor = colorSensor.getColor();
+        SmartDashboard.putNumber("xRed", detectedColor.red);
+        SmartDashboard.putNumber("xGreen", detectedColor.green);
+        SmartDashboard.putNumber("xBlue", detectedColor.blue);
+        ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+    
+        String gameData = DriverStation.getInstance().getGameSpecificMessage();
+        if (gameData.length() > 0) {
+          switch (gameData.charAt(0)) {
+          case 'B':
+            if (match.color != kRedTarget) {
+              // drive spinner motor
+            } else {
+              // stop motor
+              System.out.println("we found blue");
+    
+            }
+            SmartDashboard.putString("FMS Color", "BLUE");
+            break;
+          case 'G':
+          if (match.color != kYellowTarget) {
+              // drive spinner motor
+            } else {
+              // stop motor
+              System.out.println("we found green");
+            }
+            SmartDashboard.putString("FMS Color", "GREEN");
+            break;
+          case 'R':
+            if (match.color != kBlueTarget) {
+              // drive spinner motor
+            } else {
+              // stop motor
+              System.out.println("we found red");
+            }
+            SmartDashboard.putString("FMS Color", "RED");
+            break;
+          case 'Y':
+            if (match.color != kGreenTarget) {
+              // drive spinner motor
+            } else {
+              // stop motor
+              System.out.println("we found yellow");
+            }
+            SmartDashboard.putString("FMS Color", "YELLOW");
+            break;
+          default:
+            // This is corrupt data
+            break;
+          }
+        } else {
+          // Code for no data received yet
+        }
+    
   }
 }
